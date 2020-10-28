088173dfc2fea7dc3d8c1b29feff416d8808f4cb
Dean Lozo <dean.lozo@gmail.com>
2020-10-28 21:08:13 +0100

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,134 @@
+<script>
+    import {Input, Container, InputGroup, InputGroupAddon, InputGroupText, Button} from 'sveltestrap';
+
+    let diffRequest = {};
+    let diffResponse = {};
+
+    function getDiff() {
+        console.log(diffRequest.workspaceId)
+        console.log(diffRequest.apiKey)
+        console.log(diffRequest.startDate)
+        console.log(diffRequest.endDate)
+
+        fetch('https://work-diff.azurewebsites.net/diff', {
+            method: 'POST',
+            body: JSON.stringify(diffRequest),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if (!res.ok) {
+                throw new Error("Faaaailed!")
+            }
+            return res.json()
+        }).then(data => {
+            diffResponse.loggedHours = formatTime(data.loggedHours)
+            diffResponse.expectedHours = formatTime(data.expectedHours)
+            diffResponse.diffHours = formatTime(data.diffHours)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    function formatTime(hoursAsDecimal) {
+        // Check sign of given hoursAsDecimal
+        var sign = (hoursAsDecimal >= 0) ? 1 : -1;
+
+        // Set positive value of hoursAsDecimal of sign negative
+        hoursAsDecimal = hoursAsDecimal * sign;
+
+        // Separate the int from the decimal part
+        var hour = Math.floor(hoursAsDecimal);
+        var decpart = hoursAsDecimal - hour;
+
+        var min = 1 / 60;
+        // Round to nearest minute
+        decpart = min * Math.round(decpart / min);
+
+        var minute = Math.floor(decpart * 60) + '';
+
+        // Add padding if need
+        if (minute.length < 2) {
+            minute = '0' + minute;
+        }
+
+        // Add Sign in final result
+        sign = sign == 1 ? '' : '-';
+
+        // Concat hours and minutes
+        let time = sign + hour + ':' + minute;
+
+        return time;
+    }
+</script>
+
+<head>
+    <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    />
+    <title>Balance</title>
+</head>
+
+<Container>
+        <InputGroup class="mb-1">
+            <InputGroupAddon addonType="prepend">
+                <InputGroupText>Workspace ID</InputGroupText>
+            </InputGroupAddon>
+            <Input
+                    type="text"
+                    name="workspaceId"
+                    id="workspaceId"
+                    placeholder="Enter your workspace ID"
+                    bind:value={diffRequest.workspaceId}/>
+        </InputGroup>
+        <InputGroup class="mb-1">
+            <InputGroupAddon addonType="prepend">
+                <InputGroupText>API Key</InputGroupText>
+            </InputGroupAddon>
+            <Input
+                    type="text"
+                    name="apiKey"
+                    id="apiKey"
+                    placeholder="Enter your API Key"
+                    bind:value={diffRequest.apiKey}>
+            </Input>
+        </InputGroup>
+        <InputGroup class="mb-1">
+            <InputGroupAddon addonType="prepend">
+                <InputGroupText>Start Date</InputGroupText>
+            </InputGroupAddon>
+            <Input
+                    type="date"
+                    name="startDate"
+                    id="startDate"
+                    bind:value={diffRequest.startDate}
+            />
+        </InputGroup>
+        <InputGroup class="mb-1">
+            <InputGroupAddon addonType="prepend">
+                <InputGroupText>Start Date</InputGroupText>
+            </InputGroupAddon>
+            <Input
+                    type="date"
+                    name="endDate"
+                    id="endDate"
+                    bind:value={diffRequest.endDate}
+            />
+        </InputGroup>
+        <Button on:click={getDiff}>Have I worked enough?</Button>
+
+    <p>
+        Expected: { diffResponse.expectedHours }
+    </p>
+    <p>
+        Logged: { diffResponse.loggedHours }
+    </p>
+    <p>
+        Diff: { diffResponse.diffHours }
+    </p>
+</Container>
+
+<style>
+
+</style>
