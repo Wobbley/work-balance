f86f724e898a704dbf1065b1cf6a41d674122b79
Dean Lozo <dean.lozo@dfind.no>
2020-12-21 22:55:29 +0100

    Changed backend to the new work-diff function

--- src/diffClient.js
+++ src/diffClient.js
@@ -1,5 +1,5 @@
 export async function fetchDiff(diffRequest) {
-    let diffResponse = await fetch('https://work-diff.azurewebsites.net/diff', {
+    let diffResponse = await fetch('https://work-diff-function-20201213121219640.azurewebsites.net/api/diff', {
         method: 'POST',
         body: JSON.stringify(diffRequest),
         headers: {
